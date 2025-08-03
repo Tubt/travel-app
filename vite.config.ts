a2b869c1aa2ebc2973bc81ec7976e53e250f9858
tubt <tu.bui@gooddata.com>
2025-08-03 18:15:41 +0700

    Revert "feat: update docker run test"
    This reverts commit 89c5c08a51bad25a5a5bd0b31fc337e9b9804f28.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: true, // Listen on all local IPs
-    port: 5173,
-    strictPort: true,
-  },
-  preview: {
-    host: true,
-    port: 5173,
-    strictPort: true,
-  },
 })
